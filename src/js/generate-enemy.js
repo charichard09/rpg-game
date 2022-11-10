9e8b7284146c659d703d9e8f9932802c1ad79118
Richard <charichard09@gmail.com>
2022-11-10 14:13:15 -0800

    add code to pass generateEnemy test2
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- src/js/generate-enemy.js
+++ src/js/generate-enemy.js
@@ -4,6 +4,6 @@ export default function generateEnemy() {
   let randomChar = new Character();
   const typeArray = ['Cthulu', 'Predator', 'Space Marine'];
   randomChar.type = typeArray[Math.floor(Math.random() * 3)];
-  // this.randomChar.intelligence = Math.floor(Math.random() * 5) + 1;
+  randomChar.intelligence = Math.floor(Math.random() * 5) + 1;
   return randomChar;
 }
