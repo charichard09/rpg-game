ea7ec14d8a3aeb0b4d4a4e206c448d3425ed1577
Richard <charichard09@gmail.com>
2022-11-10 15:10:05 -0800

    add code to pass generateEnemy test2
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- __tests__/generate-enemy.tests.js
+++ __tests__/generate-enemy.tests.js
@@ -8,24 +8,39 @@ describe('generateEnemy', () => {
     expect(/(Cthulu|Predator|Space Marine)/.test(randomChar.type)).toBeTruthy();
   });
 
-  test('2. It will generate a random intelligence stat for character', () => {
-    let randomChar = generateEnemy();    
-    expect(randomChar.intelligence).toBeGreaterThanOrEqual(1);
-    expect(randomChar.intelligence).toBeLessThan(6);
+  test('2. It will distribute randomly 10 points across 3 stats', () => { 
+    let randomChar = generateEnemy();
+
+    console.log(randomChar.strength)
+    console.log(randomChar.intelligence)
+    console.log(randomChar.agility)
+    expect(randomChar.agility + randomChar.strength + randomChar.intelligence).toEqual(10);
+
+    // expect(randomChar.agility).toBeGreaterThanOrEqual(0);
+    // expect(randomChar.agility).toBeLessThanOrEqual(10);
+    // expect(randomChar.strength).toBeGreaterThanOrEqual(0);
+    // expect(randomChar.strength).toBeLessThanOrEqual(10);
+    // expect(randomChar.intelligence).toBeGreaterThanOrEqual(0);
+    // expect(randomChar.intelligence).toBeLessThanOrEqual(10);
   });
 
-  test('3. It will generate a random strength stat for character', () => {
-    let randomChar = generateEnemy();    
-    expect(randomChar.strength).toBeGreaterThanOrEqual(1);
-    expect(randomChar.strength).toBeLessThan(6);
-  });
+  // test('2. It will generate a random intelligence stat for character', () => {
+  //   let randomChar = generateEnemy();    
+  //   expect(randomChar.intelligence).toBeGreaterThanOrEqual(1);
+  //   expect(randomChar.intelligence).toBeLessThan(5);
+  // });
 
-  test('4. It will generate a random agility stat for character', () => {
-    let randomChar = generateEnemy();    
-    expect(randomChar.agility).toBeGreaterThanOrEqual(1);
-    expect(randomChar.agility).toBeLessThan(6);
-  });
+  // test('3. It will generate a random strength stat for character', () => {
+  //   let randomChar = generateEnemy();    
+  //   expect(randomChar.strength).toBeGreaterThanOrEqual(1);
+  //   expect(randomChar.strength).toBeLessThan(5);
+  // });
+
+  // test('4. It will generate a random agility stat for character', () => {
+  //   let randomChar = generateEnemy();    
+  //   expect(randomChar.agility).toBeGreaterThanOrEqual(1);
+  //   expect(randomChar.agility).toBeLessThan(5);
+  // });
 });
-
 
 
--- src/js/generate-enemy.js
+++ src/js/generate-enemy.js
@@ -4,8 +4,23 @@ export default function generateEnemy() {
   let randomChar = new Character();
   const typeArray = ['Cthulu', 'Predator', 'Space Marine'];
   randomChar.type = typeArray[Math.floor(Math.random() * 3)];
-  randomChar.intelligence = Math.floor(Math.random() * 5) + 1;
-  randomChar.strength = Math.floor(Math.random() * 5) + 1;
-  randomChar.agility = Math.floor(Math.random() * 5) + 1;
+
+  let pointTotal = 10;
+  let randomPoints1 = Math.floor(Math.random() * pointTotal) + 1;
+  let randomPoints2 = Math.floor(Math.random() * (pointTotal - randomPoints1)) + 1;
+  let randomPoints3 = pointTotal - (randomPoints2 + randomPoints1);
+
+  let randomArray = [randomPoints1, randomPoints2, randomPoints3];
+  
+
+  randomChar.agility = randomArray[Math.floor(Math.random() * 3)];
+  randomArray.splice(randomArray.indexOf(randomChar.agility), 1);
+  randomChar.strength = randomArray[Math.floor(Math.random() * 2)];
+  randomArray.splice(randomArray.indexOf(randomChar.strength), 1);
+  randomChar.intelligence = randomArray[0];
+  
+  // randomChar.intelligence = Math.floor(Math.random() * 4) + 1;
+  // randomChar.strength = Math.floor(Math.random() * 4) + 1;
+  // randomChar.agility = Math.floor(Math.random() * 4) + 1;
   return randomChar;
 }
